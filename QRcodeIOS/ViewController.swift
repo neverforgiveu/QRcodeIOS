8e663d8196a0baecd3b247d3c0ee120c8333da8a
jeffxo419 <godlikejeff@gmail.com>
2020-10-13 15:54:16 +0800

    20201013

--- QRcodeIOS/ViewController.swift
+++ QRcodeIOS/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var editText : UITextField?
+    @IBOutlet weak var imgQRCode: UIImageView!
+
+    @IBOutlet weak var btnAction:
+           UIButton!
+
+    var qrcodeImage: CIImage!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+   
+     
+    @IBAction func generateQRcode(_ sender: Any) {
+        
+        if qrcodeImage == nil{
+            if editText?.text == ""{
+                       return
+                }
+                let url = editText?.text
+                let data = url?.data(using: String.Encoding.ascii)
+                let filter = CIFilter(name: "CIQRCodeGenerator")
+                
+                filter?.setValue(data, forKey: "inputMessage")
+                filter?.setValue("H", forKey: "inputCorrectionLevel")
+                
+                 qrcodeImage = filter?.outputImage
+                 
+                 //imgQRCode.image = UIImage(ciImage: qrcodeImage) //將輸出的 CIImage 轉換成 UIImage 物件
+                  fixQR()
+                   editText?.resignFirstResponder()  //隱藏鍵盤
+                   btnAction.setTitle("清除", for: [])
+                    
+        }else{
+            imgQRCode = nil
+            qrcodeImage = nil
+            btnAction.setTitle("generate", for: [])
+        }
+       
+    }
+    
+    func fixQR() {
+        let scaleX = imgQRCode.frame.size.width / qrcodeImage.extent.size.width   //使圖片縮放比例相同
+        let scaleY = imgQRCode.frame.size.height / qrcodeImage.extent.size.height
+
+        let transformedImage = qrcodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
+
+        imgQRCode.image = UIImage(ciImage: transformedImage)
 
 
-}
+    }
+    
+    
+ 
+        
+       
+            
+        
+        
+        
+    }
+
+
+
 
